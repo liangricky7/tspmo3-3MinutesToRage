822f87a0a51bdcbad912f42bc8009abce729721b
Ricky Liang <liangricky7@gmail.com>
2026-03-21 10:03:46 -0500

    feat: add EM hold time

--- Assets/_Assets/EnergyMeter/EnergyMeter.cs
+++ Assets/_Assets/EnergyMeter/EnergyMeter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +8,17 @@ public class EnergyMeter : MonoBehaviour
     [Range(0f, 250f)] //this does nothing for capping the number; thats done in the addenergy func
     public float energy = 0f; // 0 to 250
     [SerializeField]
-    private float decayRate = 0.7f; // how much energy decays per fixed update
-
+    private float decayRate = 0.55f; // how much energy decays per fixed update
+    [SerializeField]
+    private float energyHoldTime = 2f; // how long energy holds before decaying
+    [SerializeField]
+    private float maxEnergyHoldTime = 3.5f; // how long max energy holds before decaying
+    [SerializeField]
+    private float energyHoldTimer = 0f; // timer for energy hold
+    [SerializeField]
+    private bool timerTrigger = false; // flag to check if energy is added
+    [SerializeField]
+    private bool maxEnergyHoldTrigger = false; // flag to check if max energy is reached
     public static EnergyMeter Instance { get; private set; }
 
     void Awake()
@@ -24,13 +34,50 @@ public class EnergyMeter : MonoBehaviour
         }   
     }
 
+    void Start()
+    {
+        timerTrigger = false;
+    }
+
     void FixedUpdate()
     {
-        energy = energy - decayRate >= 0 ? energy - decayRate : 0;
+        if (timerTrigger)
+        {
+            energyHoldTimer += Time.fixedDeltaTime;
+            if (maxEnergyHoldTrigger && energyHoldTimer >= maxEnergyHoldTime)
+            {
+                energyHoldTimer = 0f;
+                timerTrigger = false;
+                maxEnergyHoldTrigger = false;
+                // Debug.Log("Max energy hold");
+            }
+            else if (!maxEnergyHoldTrigger && energyHoldTimer >= energyHoldTime)
+            {
+                energyHoldTimer = 0f;
+                timerTrigger = false; 
+                // Debug.Log("normal energy hold");
+            }
+        }
+        else
+        {
+            energy = energy - decayRate >= 0 ? energy - decayRate : 0;
+        }
     }
 
     public void AddEnergy(float amount)
     {
-        energy = energy + amount > 250 ? 250 : energy + amount;
+        if (amount <= 0.1f) return; // ignore very small energy additions
+
+        if (energy + amount > 250f)
+        {
+            energy = 250f;
+            maxEnergyHoldTrigger = true; // set max energy hold trigger when max energy is reached
+        }
+        else
+        {
+            energy += amount;
+        }
+        timerTrigger = true; 
+        energyHoldTimer = 0f;
     }
 }
