fad5d35f107251aa75307304052d10e5a2b87a80
JosiahMatriarch <corbinbrescher@gmail.com>
2026-03-21 16:54:08 -0500

    initial enemy spawn script

---
+++ Assets/_Assets/enemyScripts/EnemySpawner.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("Spawn Settings")]
+    public GameObject[] enemyPrefabs;
+    public Transform player;
+    public float minSpawnRadius = 5f;
+    public float maxSpawnRadius = 15f;
+    public int maxEnemies = 10;
+    public float spawnInterval = 3f;
+
+    private float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnInterval && CountEnemies() < maxEnemies)
+        {
+            SpawnEnemy();
+            timer = 0f;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Vector2 spawnPos = GetRandomSpawnPosition();
+    }
+
+    Vector3 GetRandomSpawnPosition()
+    {
+        float theta = Random.Range(0f, Mathf.PI * 2f);
+        float phi = Mathf.Acos(Random.Range(-1f, 1f));
+        float radius = Mathf.Pow(
+            Random.Range(
+                Mathf.Pow(minSpawnRadius, 3f),
+                Mathf.Pow(maxSpawnRadius, 3f)
+            ), 1f / 3f
+        );
+
+        float x = player.position.x + radius * Mathf.Sin(phi) * Mathf.Cos(theta);
+        float y = player.position.y + radius * Mathf.Cos(phi);
+        float z = player.position.z + radius * Mathf.Sin(phi) * Mathf.Sin(theta);
+
+        return new Vector3(x, y, z);
+    }
+
+    int CountEnemies()
+    {
+        return GameObject.FindGameObjectsWithTag("EnemyGhost").Length;
+    }
+}
