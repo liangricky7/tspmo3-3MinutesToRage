3a7c4a6e9fb73d710a0e42a009efd9f7687eca82
Ricky Liang <liangricky7@gmail.com>
2026-03-22 09:37:26 -0500

    feat: new energy meter

--- Assets/_Assets/EnergyMeter/UIEnergyMeter.cs
+++ Assets/_Assets/EnergyMeter/UIEnergyMeter.cs
@@ -7,17 +7,27 @@ using UnityEngine.UI;
 public class UIEnergyMeter : MonoBehaviour
 {
     [SerializeField]
-    private TextMeshProUGUI energyText;
+    private UnityEngine.UI.Image fillImage;
+
+    public Color readyColor = new Color(0.118f, 1f, 0.973f, 1f);
+    public Color depletedColor = new Color(0.3f, 0.3f, 0.3f, 0.6f);
+
+
     // Start is called before the first frame update
     void Start()
     {
-        energyText = gameObject.GetComponentInChildren<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (EnergyMeter.Instance == null) return;
-        energyText.text = EnergyMeter.Instance.energy.ToString("F0");
+        SetFill(EnergyMeter.Instance.energy/250f);
+    }
+
+    void SetFill(float t)
+    {
+        fillImage.fillAmount = t;
+        fillImage.color = Color.Lerp(depletedColor, readyColor, t);
     }
 }
